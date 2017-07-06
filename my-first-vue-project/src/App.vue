2d7a387ef8c6ce9b18704b0760713779f2e6a67c
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-07-06 12:47:02 +0800

    小例子 v-text & v-html 的区别
    v-text & v-html 的区别

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!-- <img src="./assets/logo.png">
+    <router-view></router-view> -->
+    <!-- <h1>{{title}}</h1> -->
+    <h1 v-text = "title2"></h1>
+    <h1 v-html = "title2"></h1>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    data () {
+      return {
+        title: 'this is a todolist',
+        title2: '<span>?<span>this is a todolist - v-text & v-html 的区别'
+      }
+    }
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>

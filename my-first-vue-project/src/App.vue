9c3b02a052a779f24b48abfcc193a6d2d99dea71
MyNameIsAngela <jinlin@jinlindeMacBook-Air.local>
2017-07-06 20:46:15 +0800

    todolist 的 finish 切换 完成
    http://www.imooc.com/video/12304

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -3,9 +3,9 @@
     <!-- <h1>{{title}}</h1> -->
     <h1 v-text = "title"></h1>
     <ul>
-    <!-- 此时 <li class = "thisisLiClass"> </li> -->
-    <!-- <li v-for = "item in items" v-bind:class="liClass">  -->
-    <li v-for = "item in items" v-bind:class="{finished:item.isFinished}">
+      <!-- 此时 <li class = "thisisLiClass"> </li> -->
+      <!-- <li v-for = "item in items" v-bind:class="liClass">  -->
+      <li v-for = "item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
         {{item.label}}
       </li>
     </ul>
@@ -29,7 +29,12 @@
         ],
         liClass:'thisisLiClass'
       }
-    }
+    } ,//end data
+    methods:{
+      toggleFinish: function(item){
+        item.isFinished = !item.isFinished;
+      }
+    }//end methods
   }
 
 </script>
